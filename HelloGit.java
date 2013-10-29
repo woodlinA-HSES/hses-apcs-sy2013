9e3522a624c6d9113fa7a1ff4a81d9644d3f415f
Ayanna Woodlin <woodlinayanna@gmail.com>
2013-10-29 09:01:41 -0400

    test assignment solution

--- HelloGit.java
+++ HelloGit.java
@@ -1,7 +1,7 @@
 public class HelloGit
 {
-	public static void main(String[] args)
-	{
-		System.out.println("Hello, Git");
-	}
+ public static void main(String[] args)
+ {
+  System.out.println("Hello Git, This is Ayanna");
+ }
 }
